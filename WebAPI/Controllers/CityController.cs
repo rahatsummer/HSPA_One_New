4b2c6057aa3c19cd68a85174ddf9e3a0f87ebdd0
rahat <rahatsummer@gmail.com>
2020-10-23 07:26:51 +0600

    re

--- WebAPI/Controllers/CityController.cs
+++ WebAPI/Controllers/CityController.cs
@@ -4,7 +4,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using WebAPI.Data;
+using WebAPI.Data.Repo;
+using WebAPI.Models;
 //using WebAPI.Models;
 
 namespace WebAPI.Controllers
@@ -14,17 +17,53 @@ namespace WebAPI.Controllers
     public class CityController : ControllerBase
     {
         private readonly DataContext dc;
-        public CityController(DataContext dc)
+        private readonly ICityRepository repo;
+        public CityController(DataContext dc, ICityRepository repo)
         {
+            this.repo = repo;
             this.dc = dc;
         }
 
         // GET api/city
         [HttpGet("")]
-        public IActionResult GetCities()
+        public async Task<IActionResult> GetCities()
         {
-            var cities = dc.Cities.ToList();
+            var cities = await repo.GetCitiesAsync();
             return Ok(cities);
+        }
+
+        // POST api/city/add?cityname=Miami
+        // POST api/city/add/Los Angles
+        // [HttpPost("add")]
+        // [HttpPost("add/{cityname}")]
+
+        // public async Task<IActionResult> AddCity(string cityName)
+        // {
+        //     City city = new City();
+        //     city.Name = cityName;
+        //     await dc.Cities.AddAsync(city);
+        //     await dc.SaveChangesAsync();
+        //     return Ok(city);
+        // }
+
+        // POST api/city/post -- post the data in json format
+
+        [HttpPost("post")]
+        public async Task<IActionResult> AddCity(City city)
+        {
+            
+             repo.AddCity(city);
+            await repo.SaveAsync();
+            return StatusCode(201);
+        }
+
+        [HttpDelete("delete/{id}")]
+        public async Task<IActionResult> DeleteCity(int id)
+        {
+
+            repo.DeleteCity(id);
+            await repo.SaveAsync();
+            return Ok(id);
         }
 
 
---
+++ WebAPI/Data/Repo/CityRepository.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+
+namespace WebAPI.Data.Repo
+{
+    public class CityRepository : ICityRepository
+    {
+        private readonly DataContext dc;
+        public CityRepository(DataContext dc)
+        {
+            this.dc = dc;
+
+        }
+        public void AddCity(City city)
+        {
+           dc.Cities.Add(city);
+        }
+
+        public void DeleteCity(int CityId)
+        {
+            var cityToDelete = dc.Cities.Find(CityId);
+            dc.Cities.Remove(cityToDelete);
+        }
+
+        public async Task<IEnumerable<City>> GetCitiesAsync()
+        {
+            return await dc.Cities.ToListAsync();
+        }
+
+        public async Task<bool> SaveAsync()
+        {
+            return await dc.SaveChangesAsync() > 0;
+        }
+    }
+}
